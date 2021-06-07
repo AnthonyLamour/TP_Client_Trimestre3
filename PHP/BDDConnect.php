ee05af68647617914ec1d13b94f2003ab459f81c
AnthonyLamour <a.lamour@ludus-academie.com>
2021-06-07 10:53:03 +0200

    Ajout scripts de connexion à la BDD

---
+++ PHP/BDDConnect.php
@@ -0,0 +1,29 @@
+﻿<?php
+
+	//include du fichier de constantes
+    include 'Constantes.php';
+
+	//fonction permettant la connexion
+	function ConnecServ(){
+			
+		//mise en place du script de connexion
+			$dsn="mysql:dbname=".BASE.";host=".SERVEUR;
+			
+		//tentative de connexion à la base de données
+			try{
+				$connexion=new PDO($dsn,USER,PASSWD);
+				$connexion->exec("set names utf8");
+			}
+			
+		//affichage d'éventuelles erreurs
+			catch(PDOExecption $e){
+				printf("Echec de la connexion : %s\n", $e->getMessage());
+				exit();
+			}
+			
+		//renvoi de la connexion
+			return $connexion;
+	}
+	//connexion à la BDD
+	$conn = ConnecServ();
+?>
---
+++ PHP/Constantes.php
@@ -0,0 +1,13 @@
+<?php
+	//nom du serveur
+	define('SERVEUR',"localhost");
+
+	//nom de la base de données
+	define('BASE',"Client_TP_Trimestre3");
+
+	//nom de l'utisateur
+	define('USER',"root");
+
+	//mot de passe
+	define('PASSWD',"");
+?>
